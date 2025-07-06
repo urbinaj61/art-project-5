a74f9f3ffab69ff3b32674aef77f35f094c0d385
Joseph <joseph.urbina61@hotmail.com>
2025-07-06 14:58:16 +0200

    favourites list page completed

--- components/gallery/Cards.jsx
+++ components/gallery/Cards.jsx
@@ -1,26 +1,30 @@
 import Card from "./Card";
 
-const Cards = ({ data, handleFavouritesToggle }) => {
+const Cards = ({ data, handleFavouritesToggle, emptyArray }) => {
   return (
     <section className="art-list-container">
       <ul className="art-list">
-        {data?.map(
-          ({ slug, imageSource, name, artist, year, genre, isFavourite }) => {
-            return (
-              <li key={slug} className="art-listItem">
-                <Card
-                  imageSource={imageSource}
-                  name={name}
-                  artist={artist}
-                  slug={slug}
-                  year={year}
-                  genre={genre}
-                  isFavourite={isFavourite}
-                  handleFavouritesToggle={handleFavouritesToggle}
-                />
-              </li>
-            );
-          }
+        {emptyArray ? (
+          <h2>No favourite artists selected</h2>
+        ) : (
+          data?.map(
+            ({ slug, imageSource, name, artist, year, genre, isFavourite }) => {
+              return (
+                <li key={slug} className="art-listItem">
+                  <Card
+                    imageSource={imageSource}
+                    name={name}
+                    artist={artist}
+                    slug={slug}
+                    year={year}
+                    genre={genre}
+                    isFavourite={isFavourite}
+                    handleFavouritesToggle={handleFavouritesToggle}
+                  />
+                </li>
+              );
+            }
+          )
         )}
       </ul>
     </section>
