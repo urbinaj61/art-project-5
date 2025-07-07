32c431842888592e7afc80782276d5f6ba052d79
Joseph <joseph.urbina61@hotmail.com>
2025-07-07 16:32:47 +0200

    vercel moaning at me. Missing key :)

--- components/palette/Palette.jsx
+++ components/palette/Palette.jsx
@@ -6,7 +6,11 @@ const Palette = ({ colors }) => {
       <h3 className="art-palette-heading">Colours used in this art peice</h3>
       <section className="art-palette">
         {colors.map((colour) => (
-          <div className="art-colour" style={{ backgroundColor: colour }}></div>
+          <div
+            key={colour}
+            className="art-colour"
+            style={{ backgroundColor: colour }}
+          ></div>
         ))}
       </section>
     </>
