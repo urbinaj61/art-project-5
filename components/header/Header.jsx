fdda2ad264316eb39ba4306d0f77e73842eb60c3
Joseph <joseph.urbina61@hotmail.com>
2025-07-08 11:43:58 +0200

    all done

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,9 +1,40 @@
+import { useRouter } from "next/router";
+
 const Header = () => {
-  return (
-    <header className="art-header">
-      <h1 className="art-heading">Art Gallery</h1>
-    </header>
-  );
+  const router = useRouter();
+
+  let heading = "Art Gallery";
+  console.log(router.pathname);
+  if (router.pathname === "/")
+    heading = (
+      <>
+        <h1 className="art-heading">{heading}</h1>
+        <h3 className="art-sub-heading">Spotlight</h3>
+      </>
+    );
+  if (router.pathname === "/favourites")
+    heading = (
+      <>
+        <h1 className="art-heading">{heading}</h1>
+        <h3 className="art-sub-heading">Favourites</h3>
+      </>
+    );
+  if (router.pathname === "/gallery")
+    heading = (
+      <>
+        <h1 className="art-heading">{heading}</h1>
+        <h3 className="art-sub-heading">Art Pieces</h3>
+      </>
+    );
+  if (router.pathname === "/details/[slug]")
+    heading = (
+      <>
+        <h1 className="art-heading">{heading}</h1>
+        <h3 className="art-sub-heading">Details Page</h3>
+      </>
+    );
+
+  return <header className="art-header">{heading}</header>;
 };
 
 export default Header;
